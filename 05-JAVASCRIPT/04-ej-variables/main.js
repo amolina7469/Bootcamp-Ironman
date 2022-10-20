4b65e5d4c8570244aacbfa8807f9a811538bdace
amolina7469 <114481420+amolina7469@users.noreply.github.com>
2022-10-20 18:01:22 +0200

    c

--- 05-JAVASCRIPT/04-ej-variables/main.js
+++ 05-JAVASCRIPT/04-ej-variables/main.js
@@ -2,7 +2,6 @@ const segundos =  60
 const minutos=  60*segundos
 const horas= 24
 
-
 const segundosDia= horas * minutos
 
 console.log(`Un dia tiene ${segundosDia} segundos` )
@@ -13,6 +12,7 @@ console.log(`Un dia tiene ${segundosDia} segundos` )
 
 const euros=7
 const dolar=2
+
 const cambio= euros * dolar
 
 console.log(`7 euros son ${cambio} dolares`)
