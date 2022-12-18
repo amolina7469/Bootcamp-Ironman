f6b53f10fd006d670d31101d7920907ead76f5dd
amolina7469 <114481420+amolina7469@users.noreply.github.com>
2022-12-18 11:16:26 +0100

    commit

---
+++ 07-ANGULAR-TYPESCRIPT/Angular/marvel/src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CharactersService } from 'src/app/services/characters.service';
+import { Character } from 'src/app/interfaces/character.interface';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+characters: Character[]=[];
+
+constructor(private characterService: CharactersService){}
+
+ngOnInit() {
+  this.characterService.getAllCharacters().subscribe((data:any)=> {
+  this.characters = data.data.results;
+  console.log(this.characters);
+  });  
+}
+
+}
---
+++ 07-ANGULAR-TYPESCRIPT/Angular/marvel/src/app/services/characters.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharactersService {
+
+publicKey ='807732bbc92285931b4245c12b936035';
+hash = '37329b7a11dcd6f06371a7209876603a';
+baseUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${this.publicKey}&hash=${this.hash}`;
+
+  constructor(private httpClient : HttpClient) { }
+
+  getAllCharacters(): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl);
+}}
