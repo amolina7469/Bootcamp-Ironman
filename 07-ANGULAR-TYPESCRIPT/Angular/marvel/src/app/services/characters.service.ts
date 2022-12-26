c38892cc926e093f357bb126d7711c6499e480ac
amolina7469 <114481420+amolina7469@users.noreply.github.com>
2022-12-26 13:21:20 +0100

    pagination

--- 07-ANGULAR-TYPESCRIPT/Angular/marvel/src/app/services/characters.service.ts
+++ 07-ANGULAR-TYPESCRIPT/Angular/marvel/src/app/services/characters.service.ts
@@ -10,7 +10,8 @@ export class CharactersService {
 
 publicKey ='807732bbc92285931b4245c12b936035';
 hash = '37329b7a11dcd6f06371a7209876603a';
-baseUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${this.publicKey}&hash=${this.hash}`;
+// baseUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${this.publicKey}&hash=${this.hash}`;
+baseUrl = `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=0&ts=1&apikey=${this.publicKey}&hash=${this.hash}`;
 
   constructor(private httpClient : HttpClient) { }
 
