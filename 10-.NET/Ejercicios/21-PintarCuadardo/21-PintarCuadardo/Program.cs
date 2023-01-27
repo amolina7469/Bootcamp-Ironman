83e092e0c124c7dbc6639e404430248bc4a53871
amolina7469 <amolina7469@users.noreply.github.com>
2023-01-27 20:01:48 +0100

    commit

--- 10-.NET/Ejercicios/21-PintarCuadardo/21-PintarCuadardo/Program.cs
+++ 10-.NET/Ejercicios/21-PintarCuadardo/21-PintarCuadardo/Program.cs
@@ -14,3 +14,14 @@ for (int i = 0; i < y; i++)
     }
 
 }
+
+//Pintar rectángulo
+//for (int i = 0; i < y; i++)
+//{
+//    Console.WriteLine();
+//    for (int j = 0; j < x; j++)
+//    {
+//        Console.Write(x);
+//    }
+
+//}
